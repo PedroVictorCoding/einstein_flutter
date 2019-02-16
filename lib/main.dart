eed243a8c9454e5883a713941f54d2c2ae65468c
einstein <37004313+Impeterlewis@users.noreply.github.com>
2019-02-16 00:29:54 -0500

    einstein learning flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'einstein',
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Welcome to einstein'),
-          ),
-          body: Center(
-            child: Text('This is the first einstein screen'),
-          ),
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Einstein',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to einstein'),
         ),
-      );
+        body: Center(
+          child: Text(wordPair.asPascalCase),
+        ),
+      ),
+    );
   }
-
 }
